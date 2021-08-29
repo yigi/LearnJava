a4ce77a7c7b446f2d22057619f68fffad21c80e3
yigi <yigiterkall@gmail.com>
2021-08-29 16:46:31 +0300

    no message

--- CodeWars/WhoLikesIt.java
+++ CodeWars/WhoLikesIt.java
@@ -1,45 +1,11 @@
 public static String whoLikesIt(String... names) 
 {
-  StringBuilder message = new StringBuilder();
-  int othersSize = 0;
-  if(names.length == 0)
-  {
-    message.append("no one likes this");
-  }
-  if(names.length == 1)
-  {
-    message.append(names[0]);
-    message.append(" likes this");
-  }
-  else if(names.length == 2)
-  {
-    message.append(names[0]);
-    message.append(" and ");
-    message.append(names[1]);
-    message.append(" like this");
-  }
-  else if(names.length == 3)
-  {
-    message.append(names[0]);
-    message.append(", ");
-    message.append(names[1]);
-    message.append(" and ");
-    message.append(names[2]);
-    message.append(" like this");
-  }
-  else if( names.length > 3)
-  {
-    othersSize = names.length - 2;
-    message.append(names[0]);
-    message.append(", ");
-    message.append(names[1]);
-    message.append(" and ");
-    message.append(String.valueOf(othersSize) + " others");
-    message.append(" like this");
-  }
-  else
-  {
-    message.append("");
-  }
-  return message.toString();
+	switch (names.length) 
+	{
+          case 0: return "no one likes this";
+          case 1: return String.format("%s likes this", names[0]);
+          case 2: return String.format("%s and %s like this", names[0], names[1]);
+          case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+          default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+	}
 }
